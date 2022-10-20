2598670f2ea3134b99f56b7f6de3d8c66a5ef26d
Fuyukuniaki <info@a5-w.com>
2022-10-20 23:46:10 +0900

    [Modify] pagination周り

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ function paginations(){
             $('.next').addClass('c-size__Pagination--next c-text-size__Pagination--next c-text-color__Pagination--next c-text-text-align--right').wrap('<li class="c-mainlist__Pagination--next c-size__Pagination--next c-background__Pagination--next c-margin__Pagination--next"></li>');
         }
         else if( $(this).hasClass('dots') ) {
-            $('.dots').wrap('<li class="c-mainlist__Pagination--next c-size__Pagination--next c-margin__Pagination--next"></li>');
+            $('.dots').wrap('<li class="c-mainlist__Pagination--number c-size__Pagination--number c-text-text-align--center c-margin__Pagination--number c-background__Pagination--number c-text-color__Pagination--number"></li>');
         }
         else {
             $(this).addClass('c-size__Pagination--number').wrap('<li class="c-mainlist__Pagination--number c-size__Pagination--number c-text-text-align--center c-margin__Pagination--number c-background__Pagination--number c-text-color__Pagination--number"></li>');
@@ -55,10 +55,19 @@ function paginations(){
         $('.c-mainlist__Pagination--numerator').append('1');
         $('.c-mainlist__Pagination--denominator').append('1');
     }
+    if(window.matchMedia("(max-width: 599px)").matches){
+        if($('.c-size__Pagination--prev').length <= 0){
+            $('.p-section__Archive-list--pagination').addClass('flexEnd');
+        } else if($('.c-size__Pagination--next').length <= 0){
+            $('.p-section__Archive-list--pagination').addClass('flexStart');
+        }
+    }
 };
 
-$(window).on('load resize', function(){
+$(window).one('load', function(){
     paginations();
+});
+$(window).on('load resize', function(){
     menu_slide();
 //-------------------------------------------------------------------------
 });
